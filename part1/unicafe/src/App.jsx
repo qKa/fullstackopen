2703ae28b8f91b0da7e82200ddc5468ba5be3b73
qKa <803318+qKa@users.noreply.github.com>
2024-05-27 00:28:17 +0100

    Refactor Unicafe feedback application to use Statistics component (exercise 1.8)
    - Extract the display of statistics into a new Statistics component
    - Ensure the state of the application remains in the App root component
    - Refactor code to improve modularity and readability
    - Ensure components are defined separately, not inside other components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Statistics from "./components/Statistics";
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,28 +7,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + neutral + bad;
-  const average = total === 0 ? 0 : (good - bad) / total;
-  const positivePercentage = total === 0 ? 0 : (good / total) * 100;
-
   const handleGood = () => setGood(good + 1)
   const handleNeutral = () => setNeutral(neutral + 1)
   const handleBad = () => setBad(bad + 1)
 
-
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={ handleGood }>good</button>
-      <button onClick={ handleNeutral }>neutral</button>
-      <button onClick={ handleBad }>bad</button>
-      <h1>Statistics</h1>
-      <div>Good {good}</div>
-      <div>Neutral {neutral}</div>
-      <div>Bad {bad}</div>
-      <div>All {total}</div>
-      <div>Average {average}</div>
-      <div>Positive {positivePercentage}%</div>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
