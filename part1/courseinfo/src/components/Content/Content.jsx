fcfbd7f6deb89c577b1f792e4fa8eca3edec5f21
qKa <803318+qKa@users.noreply.github.com>
2024-05-26 19:14:25 +0100

    Refactor components for Course Information exercises 1.1 and 1.2
    - 1.1: Course Information, step 1
      - Refactor the code to create three new components: Header, Content, and Total
      - All data resides in the App component and is passed to each component via props
      - Header renders the course name
      - Content renders the parts and their number of exercises
      - Total renders the total number of exercises
    
    - 1.2: Course Information, step 2
      - Refactor the Content component to render three Part components
      - Each Part component renders the name and number of exercises for one part
      - Ensure Content component does not directly render any part names or exercise numbers

---
+++ part1/courseinfo/src/components/Content/Content.jsx
@@ -0,0 +1,13 @@
+import Part from "./ContentPart"
+
+const Content = (props) => {
+    return (
+      <div>
+        <Part name={props.part1} exercises={props.exercises1}/>
+        <Part name={props.part2} exercises={props.exercises2}/>
+        <Part name={props.part3} exercises={props.exercises3}/>
+      </div>
+    )
+  }
+
+export default Content;
---
+++ part1/courseinfo/src/components/Total/Total.jsx
@@ -0,0 +1,9 @@
+const Total = (props) => {
+    return (
+    <div>
+        <p>Number of excercises {props.exercises}</p>
+    </div>
+    )
+  }
+
+export default Total;
