1818762182e65860ec99a177253eaeca752c5d4d
qKa <803318+qKa@users.noreply.github.com>
2024-05-29 02:51:30 +0100

    Completed Phonebook step 4 (exercise 2.9)
    - Implement a search field to filter the list of people by name
      - Add an input element for the search field outside the HTML form
      - Ensure the filtering logic is case insensitive
      - Use dummy data to facilitate testing of the new functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -17,7 +21,7 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     } else {
       const newPersonObject = {
-        // id: newName,
+        id: persons.length + 1,
         name: newName,
         number: newNumber
       }
@@ -37,10 +41,22 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toUpperCase().includes(filter.toUpperCase())
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange} />
+      </div>
       <form onSubmit={addName}>
+        <h2>Add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
@@ -52,10 +68,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => {
+      {personsToShow.map(person => {
         return (
-          // <div key={person.id}>{person.name}</div>
-          <div key={person.name}>{person.name} {person.number}</div>
+          <div key={person.id}>{person.name} {person.number}</div>
         )
       })}
     </div>
