42bacf6a7b1a902ac9429ce7c752e790862ed3d2
qKa <803318+qKa@users.noreply.github.com>
2024-06-03 18:36:46 +0100

    Completed Countries step 2 (exercise 2.19)
    - Improve country information application with view buttons for each country
      - Add a button next to each country name when multiple countries are displayed
      - Show detailed view for a country when its button is pressed
      - Fetch and display detailed information (capital, area, languages, flag) for the selected country
    
    - Handle common use cases for most countries
      - Avoid focusing on edge cases like countries with names that are substrings of other countries
    
    This update enhances the user experience by allowing users to view detailed information for specific countries with a button click.

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,10 +2,13 @@ import { useState, useEffect } from 'react'
 import countryService from './services/countries'
 import Filter from './components/Filter'
 import Countries from './components/Countries'
+import CountryDetails from './components/CountryDetails'
 
 const App = () => {
   const [countries, setCountries] = useState(null)
   const [filter, setFilter] = useState('')
+  const [selectedCountryName, setSelectedCountryName] = useState(null)
+  const [selectedCountryData, setSelectedCountryData] = useState(null)
 
   useEffect(() => {
     countryService.getAll()
@@ -17,22 +20,51 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    if (selectedCountryName) {
+      countryService.getCountryByName(selectedCountryName)
+        .then(response => {
+          console.log("[getCountryByName] response:", response.data)
+          setSelectedCountryData(response.data)
+        })
+        .catch(error => {
+          console.error('[getCountryByName] Error fetching country details:', error)
+        })
+    }
+
+    return () => {
+      console.log('[getCountryByName] Cleanup function...')
+      setSelectedCountryData(null)
+      setSelectedCountryName(null)
+    }
+  }, [selectedCountryName])
+
   if (countries === null) {
     return null
   }
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
+    setSelectedCountryData(null)
   }
 
   const filteredCountries = countries.filter(country =>
     country.name.common.toUpperCase().includes(filter.toUpperCase())
   )
 
+  const onShowDetails = (countryName) => {
+    setSelectedCountryName(countryName)
+    console.log("[onShowDetails] function received countryName:", countryName)
+  }
+
   return (
     <div>
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
-      <Countries filteredCountries={filteredCountries} />
+      {selectedCountryData ? (
+        <CountryDetails country={selectedCountryData} />
+      ) : (
+        <Countries filteredCountries={filteredCountries} onShowDetails={onShowDetails} />
+      )}
     </div>
   )
 }
